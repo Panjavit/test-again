4c8bebf09e32eb1c36ff46ad39ff765347f0925a
Panjavit <123355218+Panjavit@users.noreply.github.com>
2023-07-20 14:25:06 +0700

    lab03-javafx

---
+++ lab03-javafx/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ku.cs.lab03javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ku.cs.controllers to javafx.fxml;
+    exports ku.cs.controllers;
+    exports ku.cs.lab03javafx;
+    opens ku.cs.lab03javafx to javafx.fxml;
+}
